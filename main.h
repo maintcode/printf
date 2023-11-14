ac61db4eaaa5c6f4fbfe1e4fe836a1455c2e0d39
maintcode <jmwai2007@gmail.com>
2023-11-14 15:33:51 -0800

    Print F Project

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+int handle_format(va_list args, char specifier);
+int write_char(va_list args);
+int write_str(va_list args);
+int write_int(va_list args);
+int print_number(int num);
+
+#endif /* MAIN_H */
