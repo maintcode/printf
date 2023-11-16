c904619c7c92ab3a0113c25c6868d49a570d0486
maintcode <jmwai2007@gmail.com>
2023-11-16 07:23:19 -0800

    Afternoon update on printf

--- _printf.c
+++ _printf.c
@@ -29,7 +29,6 @@ int _printf(const char *format, ...)
 		else
 		{
 			write(1, format, 1);
-
 			count++;
 		}
 
@@ -105,53 +104,4 @@ int write_str(va_list args)
 	}
 
 	return (count);
-}
-
-/**
- * write_int - Writes a signed integer to standard output
- * @args: Variable argument list
- *
- * Return: Number of characters printed
- */
-int write_int(va_list args)
-{
-	int num = va_arg(args, int);
-	int count = 0;
-
-	if (num < 0)
-	{
-		write(1, "-", 1);
-		num = -num;
-		count++;
-	}
-
-	count += print_number(num);
-	return (count);
-}
-
-/**
- * print_number - Recursive function to print
- * @num: Integer to be printed
- *
- * Return: Number of characters printed
- */
-int print_number(int num)
-{
-	int count = 0;
-	char digit;
-
-	if (num < 0)
-	{
-		write(1, "-", 1);
-		count++;
-		num = -num;
-	}
-
-	if (num / 10 != 0)
-		count += print_number(num / 10);
-
-	digit = (char)(num % 10) + '0';
-	write(1, &digit, 1);
-
-	return (count + 1);
 }
